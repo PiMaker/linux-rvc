8261a3e0e007272b0aaf7eefdb10d692bbccb83e
pi <stefan@pimaker.at>
2023-11-12 17:25:00 +0100

    rvc: extend sysfs with rng support
    replaces other on-boot rng sources, not the cleanest but should be a bit
    more supported
    
    also allows triggering via a new rvc sysfs entry

--- drivers/misc/rvc.c
+++ drivers/misc/rvc.c
@@ -5,11 +5,27 @@
 #include <linux/init.h>
 #include <linux/fs.h>
 #include <linux/string.h>
+#include <linux/random.h>
 #include <asm/csr.h>
 
 #define CSR_PLAYER_ID 0x0be
+#define CSR_RNG 0x0bf
 
 static struct kobject *rvc_kobject;
+
+// HELPERS
+
+static void add_rng_entropy(void)
+{
+    uint32_t rvc_rng, i;
+    pr_info("[_pi_] adding 16 byte of rvc randomness to entropy pool\n");
+    for (i = 0; i < (16 / sizeof(rvc_rng)); i++) {
+        rvc_rng = csr_read(CSR_RNG);
+        add_hwgenerator_randomness(&rvc_rng, sizeof(rvc_rng), sizeof(rvc_rng), false);
+    }
+}
+
+// SYSFS ENTRIES
 
 static ssize_t player_id_show(struct kobject *kobj, struct kobj_attribute *attr,
                               char *buf)
@@ -17,18 +33,17 @@ static ssize_t player_id_show(struct kobject *kobj, struct kobj_attribute *attr,
     uint32_t player_id = csr_read(CSR_PLAYER_ID);
     return sprintf(buf, "%d", player_id);
 }
+static struct kobj_attribute player_id_attribute = __ATTR_RO(player_id);
 
-static ssize_t player_id_store(struct kobject *kobj,
-                               struct kobj_attribute *attr,
-                               const char *buf, size_t count)
+static ssize_t sync_rng_store(struct kobject *kobj, struct kobj_attribute *attr,
+                              const char *buf, size_t count)
 {
-    return -EINVAL;
+    add_rng_entropy();
+    return count;
 }
+static struct kobj_attribute sync_rng_attribute = __ATTR_WO(sync_rng);
 
-
-static struct kobj_attribute player_id_attribute = __ATTR(player_id, 0444,
-                                                          player_id_show,
-                                                          player_id_store);
+// SETUP
 
 static int __init rvc_sysfs_init(void)
 {
@@ -42,10 +57,18 @@ static int __init rvc_sysfs_init(void)
     if (error) {
         pr_err("[_pi_] failed to create rvc sysfs entry 'player_id': %d\n",
                error);
-        return error;
-    } else {
-        pr_info("[_pi_] rvc sysfs entries created\n");
     }
+
+    error = sysfs_create_file(rvc_kobject, &sync_rng_attribute.attr);
+    if (error) {
+        pr_err("[_pi_] failed to create rvc sysfs entry 'sync_rng': %d\n",
+               error);
+    }
+
+    pr_info("[_pi_] rvc sysfs entries created\n");
+
+    // add some at boot
+    add_rng_entropy();
 
     return 0;
 }
